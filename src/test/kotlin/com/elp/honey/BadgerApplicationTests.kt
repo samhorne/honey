cef9710498269f76d4750139ee3422b418ade4dd
Sam Horne <samuel.horne@life.church>
2022-04-07 12:06:29 -0500

    implement tests
    findItems
    confirm error test for findItem

--- src/test/kotlin/com/elp/honey/BadgerApplicationTests.kt
+++ src/test/kotlin/com/elp/honey/BadgerApplicationTests.kt
@@ -7,11 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import io.mockk.every
 import com.ninjasquad.springmockk.MockkBean
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity.get
 import org.springframework.http.ResponseEntity.status
 import org.springframework.test.web.client.match.MockRestRequestMatchers.content
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.web.server.ResponseStatusException
 
 
 @WebMvcTest
@@ -20,14 +23,25 @@ class BadgerApplicationTests(@Autowired val mockMvc: MockMvc){
 	@MockkBean
 	lateinit var bucketListService: BucketListService
 
-	val bucketListItem = BucketListItem("1", "Paris")
+	val bucketListItems = mutableListOf(BucketListItem("1", "Paris"))
+	val bucketListItem = BucketListItem("2", "Yosemite")
 
 	@Test
 	fun exampleTest() {
-		every { bucketListService.findItem(1) } returns bucketListItem;
+		every { bucketListService.findItems() } returns bucketListItems;
+
 		mockMvc.get("/api/v1/bucketlist/")
 				.andExpect { status { isOk() } }
+				.andExpect { content { contentType(MediaType.APPLICATION_JSON) } }
+	}
 
+	@Test
+	fun testIdDoesNotExist(){
+		every { bucketListService.findItem(3) } throws ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		mockMvc.get("/api/v1/bucketlist/3")
+				.andExpect { status { isNotFound() } }
+//				.andExpect { content { contentType(MediaType.APPLICATION_JSON) } }
 	}
 
 	@Test
