326be82decb83ab958666cef6bfffe489ea61479
Sam Horne <samuel.horne@life.church>
2022-04-07 11:44:53 -0500

    implement bare-bones test
    test currently fails due to 405

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.ninja-squad:springmockk:3.0.1")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.ibm.db2:jcc")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/test/kotlin/com/elp/honey/BadgerApplicationTests.kt
+++ src/test/kotlin/com/elp/honey/BadgerApplicationTests.kt
@@ -1,13 +1,50 @@
 package com.elp.honey
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import io.mockk.every
+import com.ninjasquad.springmockk.MockkBean
+import org.springframework.http.RequestEntity.get
+import org.springframework.http.ResponseEntity.status
+import org.springframework.test.web.client.match.MockRestRequestMatchers.content
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-@SpringBootTest
-class BadgerApplicationTests {
+
+@WebMvcTest
+class BadgerApplicationTests(@Autowired val mockMvc: MockMvc){
+
+	@MockkBean
+	lateinit var bucketListService: BucketListService
+
+	val bucketListItem = BucketListItem("1", "Paris")
+
+	@Test
+	fun exampleTest() {
+		every { bucketListService.findItem(1) } returns bucketListItem;
+		mockMvc.get("/api/v1/bucketlist")
+				.andExpect { status { isOk() } }
+
+	}
 
 	@Test
 	fun contextLoads() {
+		var sum = 2 + 2
+		var answer = 4
+		assertEquals(sum, answer)
 	}
+
+//	@Test
+//	fun testMyCode(){
+//		mockMvc.get("/api/v1/bucketlist/")
+//				.andExpect {
+//					status {
+//						isOk()
+//					}
+//				}
+//	}
 
 }
